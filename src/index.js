4384ffedb2d9b3b61cb0f34d03c4e13b330f02e6
Sheriff <sheriff.azeez@globalaccelerex.com>
2023-12-10 20:57:28 +0100

    jhjk

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './Components/pages/about';
 import Home from "./Components/pages/Home"
 // import Services from "./Components/pages/services"
-import Services from "./Components/pages/services"
+// import Services from "./Components/pages/services"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,9 +15,9 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="Home" element={<Home />} />
       </Routes>
-      <Routes>
+      {/* <Routes>
         <Route path="Services" element={<Services />} />
-      </Routes>
+      </Routes> */}
       <Routes>
         <Route path="About" element={<About />} />
       </Routes>
