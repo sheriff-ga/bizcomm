3717742cff00edea2db59b88d3ae988f23ad1784
Sheriff <sheriff.azeez@globalaccelerex.com>
2024-01-06 15:56:42 +0100

    Revert "Merge branch 'gh-pages' of https://github.com/sheriff-ga/bizcomm into gh-pages"
    This reverts commit 6c2bde3db8f0de0cfc76e013b33c7173b1874d64, reversing
    changes made to 714883139b03c69d3752e8faf08ac2b7dbb344b9.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React from 'react';
+import Home from './Components/pages/Home.jsx';
+// import './assets/js/main.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
