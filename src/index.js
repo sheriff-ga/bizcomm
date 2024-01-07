1263928b5f870c5d07a3e9606dfe5071bb129921
Sheriff <sheriff.azeez@globalaccelerex.com>
2024-01-07 15:40:09 +0100

    kjkh

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/bizcomm/about" element={<App />} />
         <Route path="/" element={<Home />} />
         <Route path="/bizcomm/about" element={<About />} />
         {/* <Route path="/bizcomm/services" element={<Services />} /> */}
